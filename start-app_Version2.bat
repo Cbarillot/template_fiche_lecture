c8ceebce81742f26ecfa7799934054ebc737adae
Célia Barillot <103997877+Cbarillot@users.noreply.github.com>
2025-07-13 19:53:20 +0200

    start-app_Version3.bat

--- start-app_Version2.bat
+++ start-app_Version2.bat
@@ -30,13 +30,9 @@ set DEFAULT_PORT=5173
 set NODE_MIN_VERSION=16
 set BROWSER_DELAY=3
 set LOG_FILE=launcher.log
-set PREFERRED_BROWSER=
 
 REM Créer le fichier de log avec timestamp
 echo [%date% %time%] Launcher démarré > "%LOG_FILE%"
-
-REM Détecter le navigateur préféré
-call :detect_browser
 
 REM Fonction pour vérifier la version de Node.js
 for /f "tokens=1 delims=." %%i in ('node --version 2^>nul') do (
@@ -99,34 +95,13 @@ if %errorlevel% neq 0 (
 )
 
 REM Ouverture automatique du navigateur après un délai
-echo [INFO] Ouverture du navigateur dans %BROWSER_DELAY% secondes...
-echo [INFO] Navigateur détecté: %PREFERRED_BROWSER%
-REM Créer un script temporaire pour ouvrir le navigateur
-echo @echo off > "%TEMP%\open_browser.bat"
-echo timeout /t %BROWSER_DELAY% ^>nul >> "%TEMP%\open_browser.bat"
-if defined BROWSER_PATH (
-    if "%BROWSER_PATH%"=="" (
-        echo start http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-    ) else (
-        if /i "%PREFERRED_BROWSER%"=="Chrome" (
-            echo start "" %BROWSER_PATH% --new-window --disable-web-security --disable-features=VizDisplayCompositor http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-        ) else if /i "%PREFERRED_BROWSER%"=="Firefox" (
-            echo start "" %BROWSER_PATH% -new-window http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-        ) else if /i "%PREFERRED_BROWSER%"=="Edge" (
-            echo start "" %BROWSER_PATH% --new-window http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-        ) else if /i "%PREFERRED_BROWSER%"=="Brave" (
-            echo start "" %BROWSER_PATH% --new-window --disable-web-security http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-        ) else if /i "%PREFERRED_BROWSER%"=="Opera" (
-            echo start "" %BROWSER_PATH% --new-window http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-        ) else (
-            echo start "" %BROWSER_PATH% http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-        )
-    )
-) else (
-    echo start http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_browser.bat"
-)
-echo del "%TEMP%\open_browser.bat" >> "%TEMP%\open_browser.bat"
-start /min "%TEMP%\open_browser.bat"
+echo [INFO] Ouverture sur Google Chrome dans %BROWSER_DELAY% secondes...
+REM Créer un script temporaire pour ouvrir Chrome
+echo @echo off > "%TEMP%\open_chrome.bat"
+echo timeout /t %BROWSER_DELAY% ^>nul >> "%TEMP%\open_chrome.bat"
+echo start chrome.exe --new-window http://localhost:%DEFAULT_PORT% >> "%TEMP%\open_chrome.bat"
+echo del "%TEMP%\open_chrome.bat" >> "%TEMP%\open_chrome.bat"
+start /min "%TEMP%\open_chrome.bat"
 
 echo.
 echo ==========================================
@@ -154,139 +129,6 @@ timeout /t 2 >nul
 exit /b 0
 
 REM =================== FONCTIONS ===================
-
-:detect_browser
-echo [INFO] Détection du navigateur préféré...
-
-REM Vérifier le navigateur par défaut dans la registry
-for /f "tokens=2*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice" /v ProgId 2^>nul') do set DEFAULT_BROWSER=%%b
-
-REM Détecter le navigateur basé sur ProgId
-if defined DEFAULT_BROWSER (
-    if /i "%DEFAULT_BROWSER%"=="ChromeHTML" (
-        set PREFERRED_BROWSER=Chrome
-        set BROWSER_PATH=chrome.exe
-        call :find_chrome_path
-    ) else if /i "%DEFAULT_BROWSER%"=="FirefoxURL" (
-        set PREFERRED_BROWSER=Firefox
-        set BROWSER_PATH=firefox.exe
-        call :find_firefox_path
-    ) else if /i "%DEFAULT_BROWSER%"=="MSEdgeHTM" (
-        set PREFERRED_BROWSER=Edge
-        set BROWSER_PATH=msedge.exe
-    ) else if /i "%DEFAULT_BROWSER%"=="OperaStable" (
-        set PREFERRED_BROWSER=Opera
-        set BROWSER_PATH=opera.exe
-        call :find_opera_path
-    ) else if /i "%DEFAULT_BROWSER%"=="BraveHTML" (
-        set PREFERRED_BROWSER=Brave
-        set BROWSER_PATH=brave.exe
-        call :find_brave_path
-    ) else (
-        set PREFERRED_BROWSER=Système par défaut
-        set BROWSER_PATH=
-    )
-) else (
-    REM Fallback: chercher manuellement les navigateurs populaires
-    call :fallback_browser_detection
-)
-
-call :log_message "Navigateur détecté: %PREFERRED_BROWSER%"
-goto :eof
-
-:find_chrome_path
-REM Chercher Chrome dans les emplacements courants
-if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
-    set BROWSER_PATH="%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-) else if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
-    set BROWSER_PATH="%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
-) else if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" (
-    set BROWSER_PATH="%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
-) else (
-    set BROWSER_PATH=chrome.exe
-)
-goto :eof
-
-:find_firefox_path
-REM Chercher Firefox dans les emplacements courants
-if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" (
-    set BROWSER_PATH="%ProgramFiles%\Mozilla Firefox\firefox.exe"
-) else if exist "%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" (
-    set BROWSER_PATH="%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe"
-) else (
-    set BROWSER_PATH=firefox.exe
-)
-goto :eof
-
-:find_opera_path
-REM Chercher Opera dans les emplacements courants
-if exist "%LOCALAPPDATA%\Programs\Opera\opera.exe" (
-    set BROWSER_PATH="%LOCALAPPDATA%\Programs\Opera\opera.exe"
-) else if exist "%ProgramFiles%\Opera\opera.exe" (
-    set BROWSER_PATH="%ProgramFiles%\Opera\opera.exe"
-) else (
-    set BROWSER_PATH=opera.exe
-)
-goto :eof
-
-:find_brave_path
-REM Chercher Brave dans les emplacements courants
-if exist "%LOCALAPPDATA%\BraveSoftware\Brave-Browser\Application\brave.exe" (
-    set BROWSER_PATH="%LOCALAPPDATA%\BraveSoftware\Brave-Browser\Application\brave.exe"
-) else if exist "%ProgramFiles%\BraveSoftware\Brave-Browser\Application\brave.exe" (
-    set BROWSER_PATH="%ProgramFiles%\BraveSoftware\Brave-Browser\Application\brave.exe"
-) else (
-    set BROWSER_PATH=brave.exe
-)
-goto :eof
-
-:fallback_browser_detection
-echo [INFO] Détection manuelle des navigateurs...
-REM Ordre de préférence pour la détection
-if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" (
-    set PREFERRED_BROWSER=Chrome
-    set BROWSER_PATH="%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
-) else if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
-    set PREFERRED_BROWSER=Chrome
-    set BROWSER_PATH="%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-) else if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" (
-    set PREFERRED_BROWSER=Firefox
-    set BROWSER_PATH="%ProgramFiles%\Mozilla Firefox\firefox.exe"
-) else if exist "%LOCALAPPDATA%\BraveSoftware\Brave-Browser\Application\brave.exe" (
-    set PREFERRED_BROWSER=Brave
-    set BROWSER_PATH="%LOCALAPPDATA%\BraveSoftware\Brave-Browser\Application\brave.exe"
-) else (
-    set PREFERRED_BROWSER=Edge (par défaut)
-    set BROWSER_PATH=msedge.exe
-)
-goto :eof
-
-:open_browser
-if defined BROWSER_PATH (
-    if "%BROWSER_PATH%"=="" (
-        REM Utiliser l'ouverture système par défaut
-        start %1
-    ) else (
-        REM Utiliser le navigateur spécifique avec options de développement
-        if /i "%PREFERRED_BROWSER%"=="Chrome" (
-            start "" %BROWSER_PATH% --new-window --disable-web-security --disable-features=VizDisplayCompositor %1
-        ) else if /i "%PREFERRED_BROWSER%"=="Firefox" (
-            start "" %BROWSER_PATH% -new-window %1
-        ) else if /i "%PREFERRED_BROWSER%"=="Edge" (
-            start "" %BROWSER_PATH% --new-window %1
-        ) else if /i "%PREFERRED_BROWSER%"=="Brave" (
-            start "" %BROWSER_PATH% --new-window --disable-web-security %1
-        ) else if /i "%PREFERRED_BROWSER%"=="Opera" (
-            start "" %BROWSER_PATH% --new-window %1
-        ) else (
-            start "" %BROWSER_PATH% %1
-        )
-    )
-) else (
-    REM Fallback vers l'ouverture système
-    start %1
-)
-goto :eof
 
 :log_message
 echo [%date% %time%] %~1 >> "%ORIGINAL_DIR%\%LOG_FILE%"
