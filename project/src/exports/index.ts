0e56a83ede324c58698e27cba610536f7367c7b7
Célia Barillot <103997877+Cbarillot@users.noreply.github.com>
2025-08-26 21:57:00 +0200

    Deleted launch.json

--- project/src/exports/index.ts
+++
@@ -1,6 +0,0 @@
-export { exportSimple } from './simple';
-export { exportModern } from './modern';
-export { exportWebStyle } from './webstyle';
-export { exportSimpleWeb } from './simple-web';
-export { downloadFile, openFileInNewTab } from './utils';
-export * from './types';
